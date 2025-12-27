56132a2e6cc713c8aa0ee5194610a95ba5f4043e
Muthu Rajesh T <muthurajesht041@gmail.com>
2025-12-27 19:39:29 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./**/*.{js,ts,jsx,tsx}", // Added this to look everywhere
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
